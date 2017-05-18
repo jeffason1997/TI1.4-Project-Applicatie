8ff0e9f960ce86bda6584a8cc87dfecbe0f2cd4a
JeffreyLantinga <jeffreylantinga@hotmail.com>
2017-05-18 10:14:15 +0200

    The Project

---
+++ app/src/main/java/com/b2/projectgroep/ti14_project_applicatie/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.b2.projectgroep.ti14_project_applicatie;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
